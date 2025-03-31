974f0395768e2dfbe4dfa34e0fd6d30e8be44793
Junior Marquiore <juniormarquiore@gmail.com>
2025-03-31 20:41:15 -0300

    Reconfigurar GitHub Pages para mostrar o site diretamente na raiz em vez da página de redirecionamento

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/marquiorefilms' : '',
   images: {
     domains: ['i.imgur.com'], // Adiciona suporte a imagens do Imgur
     unoptimized: process.env.NODE_ENV === 'production', // Necessário para exportação estática
