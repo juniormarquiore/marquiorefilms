42110db7c7822dd47fd20d6b6c3ce85a8e6d8a81
juniormarquiore <juniormarquiore01@gmail.com>
2025-04-01 13:19:32 -0300

    Criar novo painel administrativo estilo WordPress

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/marquiorefilms',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/marquiorefilms' : '',
   trailingSlash: true,
 };
 
