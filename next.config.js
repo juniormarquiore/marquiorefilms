b92e641e82f7e76803ab2c1664a886a30c33edfd
Junior Marquiore <juniormarquiore@gmail.com>
2025-03-31 21:03:10 -0300

    Corrigir erro 404: simplificar caminhos de redirecionamento e remover referências a app/page que não existem

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Sempre exportar como estático
-  distDir: 'out',
+  output: 'export',
   images: {
-    domains: ['i.imgur.com'], // Apenas suporte a imagens do Imgur
-    unoptimized: true, // Necessário para exportação estática
+    unoptimized: true,
   },
-  // Configuração para o GitHub Pages
-  basePath: '', // Sem basePath para funcionar na raiz
-  assetPrefix: '',
 };
 
 module.exports = nextConfig;
